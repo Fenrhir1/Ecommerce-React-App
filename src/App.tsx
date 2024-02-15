9361090e93171992c9d014a8b763e0ecf0b9031c
giorgio sancarlo <sancarlogiorgio@gmail.com>
2024-02-15 14:01:26 +0100

    feat: mergedUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import Grid from "@mui/material/Grid";
@@ -14,6 +14,52 @@ function App() {
   const handleAddToCart = () => {
     setCartItems(cartItems + 1);
   };
+  function ProductList() {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
+
+    useEffect(() => {
+      const getProducts = async () => {
+        const response = await fetch(
+          "https://mockend.up.railway.app/api/products"
+        );
+        const data = await response.json();
+        setProducts(data);
+      };
+
+      getProducts();
+    }, []);
+
+    if (products)
+      return (
+        <ul>
+          {products.map(
+            (product: {
+              id: number;
+              description: string;
+              title: string;
+              price: number;
+              image: string;
+              qty: number;
+            }) => (
+              <div key={product.id}>
+                <p>{product.title}</p>
+                <p>{product.description}</p>
+                <img
+                  style={{ width: "100px", height: "100px" }}
+                  src={product.image}
+                  alt={product.title}
+                />
+                <p>Prezzo:{product.price}</p>
+                <p>Quantità:{product.qty}</p>
+                <button>Add to cart</button>
+              </div>
+            )
+          )}
+        </ul>
+      );
+    else return <p>Loading...</p>;
+  }
 
   return (
     <Paper
@@ -58,6 +104,9 @@ function App() {
         <Typography variant="body1" sx={{ textAlign: "center", mt: 2 }}>
           {description}
         </Typography>
+      </Grid>
+      <Grid>
+        <ProductList />
       </Grid>
     </Paper>
   );
