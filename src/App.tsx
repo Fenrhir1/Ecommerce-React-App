6c30d397f35344b262fe208691212ec0cf46645d
Salvatore Polizzotti <fenrhir@libero.it>
2024-02-15 12:07:50 +0100

    created logic for fetch API

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const fibonacci = (n: number): number[] => {
-    const sequence: number[] = [0, 1];
-    for (let i = 2; i < n; i++) {
-      sequence.push(sequence[i - 1] + sequence[i - 2]);
-    }
-    return sequence;
-  };
+  function ProductList() {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
-  const fibSequence = fibonacci(10); // Change the number to generate a different sequence
+    useEffect(() => {
+      const getProducts = async () => {
+        const response = await fetch(
+          "https://mockend.up.railway.app/api/products"
+        );
+        const data = await response.json();
+        setProducts(data);
+      };
+
+      getProducts();
+    }, []);
+
+    if (products)
+      return (
+        <ul>
+          {products.map(
+            (product: {
+              id: number;
+              description: string;
+              title: string;
+              price: number;
+              image: string;
+              qty: number;
+            }) => (
+              <div key={product.id}>
+                <p>{product.title}</p>
+                <p>{product.description}</p>
+                <img
+                  style={{ width: "100px", height: "100px" }}
+                  src={product.image}
+                  alt={product.title}
+                />
+                <p>Prezzo:{product.price}</p>
+                <p>Quantità:{product.qty}</p>
+                <button>Add to cart</button>
+              </div>
+            )
+          )}
+        </ul>
+      );
+    else return <p>Loading...</p>;
+  }
 
   return (
-    <div className="App">
-      <ul>
-        {fibSequence.map((num, index) => (
-          <li key={index}>{num}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>App</h1>
+      <ProductList />
     </div>
   );
 }
